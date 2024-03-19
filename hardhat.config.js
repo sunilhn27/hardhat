173e82ff70345b9dc70a5f29119679e03412bbb4
Sunil H N <sunilh.n27@gmail.com>
2024-03-20 00:36:25 +0530

    added localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ task("accounts", "Print all accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
   for (const account of accounts) {
     const address = await account.getAddress();
+    console.log(accounts);
     const bal = await hre.ethers.provider.getBalance(address)
     console.log(address + " " + hre.ethers.formatEther(bal));
   }
@@ -26,6 +27,11 @@ module.exports = {
     sepolia: {
       url: process.env.API_KEY,
       accounts: [process.env.PRIVATE_KEY]
+    },
+    local: {
+      url: "http://localhost:8545", // Local network URL
+      chainId: 1337, // Assuming it's the same as Hardhat's default chain ID
+      accounts: ["0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"]
     }
   },
 
