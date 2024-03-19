c2c38ba1bba88ec0b27b5d921038a55de265120c
Sunil H N <sunilh.n27@gmail.com>
2024-03-20 00:35:54 +0530

    Added test for Greet

--- test/Greet.js
+++ test/Greet.js
@@ -5,12 +5,28 @@ const { describe } = require("mocha");
 
 describe("Greeter", async () => {
 
-    it("Should check the new greet Value", async () => {
+    let contract;
+
+    beforeEach(async () => {
         const Greeter = await ethers.getContractFactory("Greeter");
         const greeter = await Greeter.deploy("Hello Sunil");
-        console.log(await greeter.greet())
-        await greeter.setGreet("Ola Sunil")
-        expect(await greeter.greet()).to.equal("Ola Sunil")
+        contract = greeter;
+    })
+
+    it("Should check the new greet Value", async () => {
+
+        console.log(await contract.greet())
+        await contract.setGreet("Ola Sunil")
+        expect(await contract.greet()).to.equal("Ola Sunil")
+
+
+    })
+
+    it("Should check the new greet Value", async () => {
+
+        console.log(await contract.greet())
+        await contract.setGreet("Ola Sunil")
+        expect(await contract.greet()).to.equal("Ola Sunil")
 
 
     })
