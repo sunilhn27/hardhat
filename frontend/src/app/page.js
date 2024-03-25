8625f2050891652fe9fe2798257dc45353f74d3d
Sunil H N <sunilh.n27@gmail.com>
2024-03-25 23:53:49 +0530

    Changes in Page component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,10 @@
-import Homepage from "@/Components/Homepage";
-import Image from "next/image";
-
+import Homepage from "../Components/Homepage";
+import Hero from "../components/Hero"
 export default function Home() {
   return (
     <main>
       <Homepage />
+      <Hero />
     </main>
   );
 }
